ba1e2da0dca93a8a8fd6ba37dfbac24eedf0a4d5
grantimbo <grant.imbo@gmail.com>
2020-10-29 04:51:10 +0800

    mobile display fix

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <footer class="pt-3 pb-3">
     <div class="container">
         <div class="row pt-3">
-            <div class="col-lg-6 text-left">
-                <a href="index.html" class="mb-2 d-inline">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="title" class="logo" width="120">
+            <div class="col-lg-6">
+                <a href="index.html" class="d-inline">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="title" class="logo mb-3" width="120">
                 </a>
             </div>
-            <div class="col-lg-6 copyright text-right">
+            <div class="col-lg-6 copyright">
                 <p>Copyright @ <?php echo date('Y'); ?> - Rettun LLC. All Rights Reserved</p>
                 <div class="privacy-links">
                     <?php footer_links(); ?>
